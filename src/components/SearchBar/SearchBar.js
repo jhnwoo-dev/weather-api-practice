706c4ea1e991df857ff86fca220e6bb27193cc65
Jason Nguyen <jhnwoo.dev@gmail.com>
2023-06-05 14:33:27 -0700

    implementation of async await

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -12,42 +12,7 @@ function SearchBar({
     const [long, setLong] = useState('')
 
     //Callback Implementation
-    // function GetDataCallback(lat, long, cb) {
-    //     let request = new XMLHttpRequest();
-    //     const api_url = `https://api.open-meteo.com/v1/dwd-icon?latitude=${lat}&longitude=${long}&hourly=temperature_2m&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch`
-
-    //     request.open("GET", api_url);
-    //     request.onload = function () {
-    //         if (request.status === 200) {
-    //             let data = JSON.parse(request.response)
-    //             cb(data);
-    //         } else {
-    //             cb(new Error(`ERROR: ${request.status} - Please input valid latitude and longitude coordinates.`))
-    //         }
-    //     }
-    //     request.send();
-    // }
-
-    //Promise Implementation
-    // function GetDataPromises(lat, long) {
-    //     let request = new XMLHttpRequest();
-    //     const api_url = `https://api.open-meteo.com/v1/dwd-icon?latitude=${lat}&longitude=${long}&hourly=temperature_2m&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch`
-
-    //     return new Promise((resolve, reject) => {
-    //         request.open("GET", api_url);
-    //         request.onload = function () {
-    //             if (request.status === 200) {
-    //                 let data = JSON.parse(request.response)
-    //                 resolve(data);
-    //             } else {
-    //                 reject(new Error(`ERROR: ${request.status} - Please input valid latitude and longitude coordinates.`))
-    //             }
-    //         }
-    //         request.send();
-    //     })
-    // }
-
-    async function GetDataAsync() {
+    function GetDataCallback(lat, long, cb) {
         let request = new XMLHttpRequest();
         const api_url = `https://api.open-meteo.com/v1/dwd-icon?latitude=${lat}&longitude=${long}&hourly=temperature_2m&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch`
 
@@ -55,16 +20,35 @@ function SearchBar({
         request.onload = function () {
             if (request.status === 200) {
                 let data = JSON.parse(request.response)
-                return data
+                cb(data);
             } else {
-                reject(new Error(`ERROR: ${request.status} - Please input valid latitude and longitude coordinates.`))
+                cb(new Error(`ERROR: ${request.status} - Please input valid latitude and longitude coordinates.`))
             }
         }
         request.send();
-
     }
 
-    function callAPI() {
+    //Promise Implementation
+    function GetDataPromises(lat, long) {
+        let request = new XMLHttpRequest();
+        const api_url = `https://api.open-meteo.com/v1/dwd-icon?latitude=${lat}&longitude=${long}&hourly=temperature_2m&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch`
+        request.open("GET", api_url);
+
+        return new Promise((resolve, reject) => {
+            request.onload = function () {
+                if (request.status === 200) {
+                    let data = JSON.parse(request.response)
+                    resolve(data);
+                } else {
+                    reject(new Error(`ERROR: ${request.status} - Please input valid latitude and longitude coordinates.`))
+                }
+            }
+            request.send();
+        })
+    }
+
+    // Async/Await Implementation
+    async function callAPI() {
         //Callback Implementation
         // GetDataCallback(lat, long, (data) => {
         //         setTemp(data.current_weather.temperature)
@@ -80,7 +64,9 @@ function SearchBar({
         // })
 
         //Async/Await Implementation
-
+        let data = await GetDataPromises(lat, long)
+        setTemp(data.current_weather.temperature)
+        setWind(data.current_weather.windspeed)
     }
 
     return (
